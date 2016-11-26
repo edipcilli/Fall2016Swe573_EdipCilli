b9c62ca88516b199dbd7d88b3db64019312a1cee
Edip Cilli <edipcilli@hotmail.com>
2016-11-26 16:14:37 +0200

    First project commit

---
+++ src/main/java/com/ec/htracker/TrackerController.java
@@ -0,0 +1,19 @@
+package com.ec.htracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TrackerController {
+    @RequestMapping("/home")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="Edip") String name) {
+            	
+    	String surname = "Çilli";
+    	
+    	model.addAttribute("name", name);
+    	model.addAttribute("surname", surname);
+        return "home";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Health Tracker Login Page</title>
+</head>
+<body>
+    Hello Mahmut
+</body>
+</html>
